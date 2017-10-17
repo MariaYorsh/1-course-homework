d596356cb539d5e7da3aa4cadb3efcac2ef6eafe
MARIA <31701910+MariaYorsh@users.noreply.github.com>
2017-10-17 22:38:27 +0300

    Add files via upload

---
+++ 3task.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long s1 = 0, s2 = 1, nexts = 0, n;
+	cout << "Enter number n:";
+	cin >> n;
+	nexts = s1 + s2;
+	while (nexts <= n)
+	{
+		s1 = s2;
+		s2 = nexts;
+		nexts = s1 + s2;
+		if (nexts >= n)
+		{
+			cout << "Fibonacci number is >= n: ";
+			cout << nexts <<endl ;
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
