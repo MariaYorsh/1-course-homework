10a23db95dcb7e60bc033cb3c241b63cda7fb476
MARIA <1302506@gmail.com>
2017-10-24 20:18:37 +0300

    Revert "Merge branch 'master' of https://github.com/MariaYorsh/1-course-homework"
    This reverts commit 5ec2ba3a8138b2904a04c4da47a3df0739ecc24c, reversing
    changes made to a0f24ceba4026fe5d3a78feb8392ecffbd1ca680.

--- 1task.cpp
+++ 1task.cpp
@@ -1,5 +1,4 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -32,40 +31,6 @@ int Digit(long s, int d)
 	}
 	return t;
 }
-=======
-#include <iostream>
-
-using namespace std;
-int Digit(long s, int d);
-
-int main()
-{
-	long long n;
-	cout << "Enter number n:";
-	cin >> n;
-	for (int i = 0; i <= 9; i++)
-	{
-		cout << i << " -> " << Digit(n, i) << endl;
-	}
-
-	system("pause");
-	return 0;
-}
-int Digit(long s, int d)
-{
-	int t = 0;
-	while (s > 0)
-	{
-		int a = s % 10;
-		s = s / 10;
-		if (a == d)
-		{
-			t = t + 1;
-		}
-	}
-	return t;
-}
->>>>>>> 2a198dd2ed34e0d6b7420164e1100becf5a3f667
 =======
 #include <iostream>
 
--- 3task.cpp
+++ 3task.cpp
@@ -1,5 +1,4 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -24,32 +23,6 @@ int main()
 	}
 	system("pause");
 	return 0;
-=======
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	long s1 = 0, s2 = 1, nexts = 0, n;
-	cout << "Enter number n:";
-	cin >> n;
-	nexts = s1 + s2;
-	while (nexts <= n)
-	{
-		s1 = s2;
-		s2 = nexts;
-		nexts = s1 + s2;
-		if (nexts >= n)
-		{
-			cout << "Fibonacci number is >= n: ";
-			cout << nexts <<endl ;
-			break;
-		}
-	}
-	system("pause");
-	return 0;
->>>>>>> 2a198dd2ed34e0d6b7420164e1100becf5a3f667
 =======
 #include <iostream>
 
